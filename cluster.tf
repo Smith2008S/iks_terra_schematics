3ed6784222671649fb900dcb25ce5e37322d287f
Smith2008S <60987042+Smith2008S@users.noreply.github.com>
2020-04-18 16:55:28 -0500

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -6,7 +6,7 @@
 resource ibm_container_cluster classic_cluster {
 
     name              = "${var.cluster_name}"
-    resource_group_id = "${var.resource_group_id}"
+    resource_group_id = "5b0134601c214ccd90ab9221227c56e6"
     datacenter        = "${element(var.zones, 0)}"
     machine_type      = "${var.machine_type}"
     kube_version      = "${var.kube_version}"
@@ -17,21 +17,3 @@ resource ibm_container_cluster classic_cluster {
 
 }
 
-#############################################################################
-
-
-#############################################################################
-# Create multizone attachments for cluster
-#############################################################################
-
-resource ibm_container_worker_pool_zone_attachment multi_zone {
-    count             = "${length(var.zones) - 1}"
-    resource_group_id = "${var.resource_group_id}"
-    cluster           = "${ibm_container_cluster.classic_cluster.name}"
-    worker_pool       = "${ibm_container_cluster.classic_cluster.worker_pools.0.id}"
-    zone              = "${element(var.zones, count.index + 1)}"
-    public_vlan_id    = "${var.private_only ? null : element(var.public_vlan_ids, count.index + 1)}"
-    private_vlan_id   = "${element(var.private_vlan_ids, count.index + 1)}"
-}
-
-#############################################################################
