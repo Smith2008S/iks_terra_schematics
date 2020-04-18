9567047e9c3b08297840e46abe840bf1ccdcadd8
Smith2008S <60987042+Smith2008S@users.noreply.github.com>
2020-04-18 16:42:27 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -25,13 +25,13 @@ variable ibm_region {
 
 variable public_vlan_ids {
   description = "List of public vlans. The first will be for the master, any additional will be for worker pools"
-  type        = "list"
+  type        = "string"
 
 }
 
 variable private_vlan_ids {
   description = "List of private vlans The first will be for the master, any additional will be for worker pools"
-  type        = "list"
+  type        = "string"
 }
 
 
