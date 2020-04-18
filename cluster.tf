4027021c0eb90f65c27ea9df9948863832b5c029
Smith2008S <60987042+Smith2008S@users.noreply.github.com>
2020-04-18 17:22:40 -0500

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -5,15 +5,15 @@
 
 resource ibm_container_cluster classic_cluster {
 
-    name              = "${var.cluster_name}"
+    name              = "$${var.cluster_name}"
     resource_group_id = "5b0134601c214ccd90ab9221227c56e6"
-    datacenter        = "${element(var.zones, 0)}"
-    machine_type      = "${var.machine_type}"
-    kube_version      = "${var.kube_version}"
-    hardware          = "${var.hardware}"
-    public_vlan_id    = "${var.private_only ? null : element(var.public_vlan_ids, 0) }"
-    private_vlan_id   = "${element(var.private_vlan_ids, 0)}"
-    default_pool_size = "${var.default_pool_size}"
+    datacenter        = "$${element(var.zones, 0)}"
+    machine_type      = "$${var.machine_type}"
+    kube_version      = "$${var.kube_version}"
+    hardware          = "$${var.hardware}"
+    public_vlan_id    = "$${var.private_only ? null : element(var.public_vlan_ids, 0) }"
+    private_vlan_id   = "$${element(var.private_vlan_ids, 0)}"
+    default_pool_size = "$${var.default_pool_size}"
 
 }
 
