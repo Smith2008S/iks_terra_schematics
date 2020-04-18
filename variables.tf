d49f25a62f8a05a46951fd128412fbdef76dbb80
Smith2008S <60987042+Smith2008S@users.noreply.github.com>
2020-04-18 17:03:54 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,10 +6,6 @@ variable ibmcloud_api_key {
     description = "IBM Cloud IAM API Key"
 }
 
-variable resource_group {
-    description = "Name of resource group to provision resources"
-}
-
 variable ibm_region {
     description = "IBM Cloud region where all resources will be deployed"
     default     = "us-south"
